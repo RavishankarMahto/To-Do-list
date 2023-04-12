ed38420deb402ce6e62000de1c3cb6d0f1ccfbac
Ravishankar Mahto <ravishankarkumar326@yahoo.com>
2023-04-13 00:34:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const toDoItems = document.getElementsByClassName("to-do-items")[0];
+const input = document.getElementById("input");
+const trashIcon = document.getElementById("trash");
+
+input.addEventListener("keydown", function(event){
+    if(event.key ===  "Enter")
+    addItem();
+})
+
+function addItem(){
+    var divParent = document.createElement("div");
+    var divChild = document.createElement("div");
+    var checkIcon = document.createElement("i");
+    var trashIcon = document.createElement("i");
+
+    divParent.className = "item";
+    divParent.innerHTML = '<div>'+input.value+'</div>';
+
+    checkIcon.className ="fa-sharp fa-solid fa-square-check";
+    checkIcon.style.color = "gray";
+    checkIcon.addEventListener("click", function(){
+        checkIcon.style.color = "skyblue";
+    })
+
+    divChild.appendChild(checkIcon);
+
+    trashIcon.className = "fas fa-trash";
+    trashIcon.style.color = "darkgray";
+    trashIcon.addEventListener("click", function(){
+        divParent.remove();
+    })
+
+    divChild.appendChild(trashIcon);
+
+    divParent.appendChild(divChild);
+
+    toDoItems.appendChild(divParent);
+
+    input.value = '';
+
+}
